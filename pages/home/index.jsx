8b36e8ec9e85dfcb7b30f7fe6ba9f68586c84f8b
esraaagul <esrglesr@gmail.com>
2025-02-02 04:58:33 +0300

    add product page

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Campaigns from "@/components/Campaigns";
 import Carousel from "@/components/Carousel";
 import MenuWrapper from "@/components/product/MenuWrapper";
@@ -8,6 +9,7 @@ export default function Home() {
       <Carousel />
       <Campaigns />
       <MenuWrapper />
+      <About />
     </div>
   );
 }
